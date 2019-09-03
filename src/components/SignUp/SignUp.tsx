4ac3d76784d21d42ea87e7ab338f0be46243ecac
Apple-yong <748840808@qq.com>
2019-09-03 15:54:40 +0800

    value不能输入，只能用defaultValue

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -39,15 +39,15 @@ class SignUp extends React.Component<any,ISignUpstate> {
                 <Input
                     placeholder="请输入用户名"
                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    value={account}
+                    defaultValue ={account}
                     suffix={
                         <Tooltip title="Extra information">
                             <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
                         </Tooltip>
                     }
                 />
-                <Input.Password value={password} placeholder="请输入密码" />
-                <Input.Password value={passwordConformation} placeholder="请确认密码" />
+                <Input.Password defaultValue ={password} placeholder="请输入密码" />
+                <Input.Password defaultValue ={passwordConformation} placeholder="请确认密码" />
                 <Button onClick={this.submit}>注册</Button>
             </div>
         );
