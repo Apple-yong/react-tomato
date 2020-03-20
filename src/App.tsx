f459f3fd44826ec7b3ac98fd98351409aea7b225
Apple-yong <748840808@qq.com>
2020-03-20 17:41:25 +0800

    跳转

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import Login from './components/Login/Login'
 import Index from './components/Index/Index'
 import SignUp from './components/SignUp/SignUp'
 
-function App() {
-    return (
-        <Router>
-            <div>
-                <Route exact={true} path="/" component={Index} />
-                <Route path="/login" component={Login} />
-                <Route path="/signUp" component={SignUp} />
-            </div>
-        </Router>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact={true} path="/" component={Index} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signUp" component={SignUp} />
+                </div>
+            </Router>
+        )
+    }
 }
 
 
--- src/components/Index/Index.tsx
+++ src/components/Index/Index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Button } from 'antd';
 
-function Component() {
-    return (
-        <div className="Component">
-            <Button>登录</Button>
-        </div>
-    );
+interface IRouter {
+    history: any;
+}
+
+class Component extends React.Component<IRouter> {
+    constructor(props: any){
+        super(props)
+    }
+    login = () =>{
+        this.props.history.push('login')
+    }
+    render() {
+        return (
+            <div className="Component">
+                <Button onClick={this.login}>登录</Button>
+            </div>
+        )
+    }
+    
 }
 
 export default Component;
